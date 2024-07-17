a8a77a46f3cd334194739bb6c74ec2d2add546e5
Antonio Vuono <antoniosvuono@icloud.com>
2024-07-16 22:02:55 -0300

    chore: eslint configured

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/', () => {
-    return { hello: 'Welcome, world'}
-});
+  return { hello: 'Welcome, world' }
+})
 
-app.listen({ port: 3333}).then(() => {
-    console.log('Server is running on port 3333')
-}) 
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running on port 3333')
+})
