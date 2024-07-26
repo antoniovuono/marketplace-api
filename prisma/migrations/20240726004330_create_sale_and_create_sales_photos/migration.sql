1ba4218be9629ff159d1f81ccaefd880b4d7017b
Antonio Vuono <antoniosvuono@icloud.com>
2024-07-25 21:48:06 -0300

    feature: sales model and sales-photos model created

---
+++ prisma/migrations/20240726004330_create_sale_and_create_sales_photos/migration.sql
@@ -0,0 +1,38 @@
+-- CreateEnum
+CREATE TYPE "Condition" AS ENUM ('NOVO', 'USADO');
+
+-- CreateEnum
+CREATE TYPE "PaymentMethods" AS ENUM ('BOLETO', 'PIX', 'DINHEIRO', 'CARTAO', 'DEPOSITO');
+
+-- CreateTable
+CREATE TABLE "sales" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "condition" "Condition" NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "accept_swap" BOOLEAN NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "payment_methods" "PaymentMethods" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "sales_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Photo" (
+    "id" TEXT NOT NULL,
+    "sale_id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Photo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "sales" ADD CONSTRAINT "sales_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Photo" ADD CONSTRAINT "Photo_sale_id_fkey" FOREIGN KEY ("sale_id") REFERENCES "sales"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
